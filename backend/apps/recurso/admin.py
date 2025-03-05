481c33ec7a869f46f92718d4b87217ef2b00cae3
rastor65 <63560982+rastor65@users.noreply.github.com>
2025-03-05 16:09:28 -0500

    Delete backend directory

--- backend/apps/recurso/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.recurso.models import *
-# Register your models here.
-
-admin.site.register(Recursos)
-admin.site.register(Rol)
-admin.site.register(RecursosxRol)
-admin.site.register(RolxUsuario)
--- backend/apps/usuario/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.usuario.models import *
-# Register your models here.
-
-admin.site.register(Tipo_Documento)
-admin.site.register(Tipo_Genero)
-admin.site.register(Usuarios)
-admin.site.register(UsuarioxLicencia)
